f8873994078663b1d623e40c18ad46df583bd54e
Aakash <aakash@bptn.com>
2024-09-25 15:31:28 -0400

    prints the length of the string.

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -3,6 +3,7 @@ package com.bptn.course._05_strings;
 public class Strings {
 
 	public static void main(String[] args) {
-		
+		String userName = "testuser";
+		System.out.println(userName.length());
 	}
 }
